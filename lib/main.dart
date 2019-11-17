ee1da0effd99d4432abf514b9704ade7b1c04024
zhoujq <11918494990@qq.com>
2019-11-17 10:43:22 +0800

    add SliverAppBar、SnackBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:english_words/english_words.dart' as prefix0;
-import 'package:first_flutter_app/l_ButtonBar.dart';
-import 'package:first_flutter_app/l_FlexibleSpaceBar.dart';
+import 'package:first_flutter_app/l_SnackBar.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-
 
 //void main() => runApp(MyApp());
 // void main() => runApp(First());
@@ -12,14 +10,15 @@ import 'package:english_words/english_words.dart';
 // void main() => runApp(LAppbar());
 // void main() => runApp(LBottomAppBar());
 // void main() => runApp(LButtonBar());
-void main() => runApp(LFlexibleSpaceBar());
+// void main() => runApp(LFlexibleSpaceBar());
+// void main() => runApp(LSliverAppBar());
+void main() => runApp(LSnackBar());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      title:'Welcome to Flutter',
+      title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Startup Name Generator'),
@@ -31,38 +30,40 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class RandomWordsState extends State<RandomWords> {
   //创建一个数组
-   final _suggestions = <WordPair>[];
-   final _biggerFont = const TextStyle(fontSize:18.0);
-   Widget _buildSuggestions(){
-     return ListView.builder(padding:const EdgeInsets.all(16.0)
-         ,itemBuilder: (context,i){
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
 
-            if(i.isOdd) return Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(prefix0.generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
 
-            final index = i~/2;
-            if(index>=_suggestions.length){
-              _suggestions.addAll(prefix0.generateWordPairs().take(10));
-            }
-            return _buildRow(_suggestions[index]);
-         }
-     );
-   }
-   Widget _buildRow(WordPair pair){
-     return ListTile(
-       title: Text(
-         pair.asPascalCase,
-         style: _biggerFont,
-       ),
-     );
-   }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return _buildSuggestions();
   }
 }
+
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
